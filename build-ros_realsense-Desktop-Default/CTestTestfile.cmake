9c940b0020da316eb1fcd15933a793e211ee1e3c
jamessergeant <james_sergeant@hotmail.com>
2016-03-10 12:41:36 +1000

    Removed Qt Creator files - the sequel

--- build-ros_realsense-Desktop-Default/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/james/ros_ws/src/HARVEYapc/ros_realsense
-# Build directory: /home/james/ros_ws/src/HARVEYapc/build-ros_realsense-Desktop-Default
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-SUBDIRS(gtest)
